56b63029c644c33e99b547dd6a1f505788409d2e
G. Pierce Lusk <gpiercelusk@gmail.com>
2019-03-10 18:54:33 -0400

    changed html again , finally some functional JS

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,75 +1,122 @@
-$(document).ready(function () {
-
-  var rightAnswers = 0;
-  var wrongAnswers = 0;
-  var notAnswered = 0;
-  var timeLeft = 20;
-  var intervalId;
-  var questionAndAnswer = 0;
-  var isAnswered = false;
-  var right;
-
-  var triviaGame = [{
+var triviaQuestions = [
+  {
     question: "What is Leonardo's bandana color?",
-    answerOptions: ["Green", "Blue", "Purple", "Orange"],
-    correctAnswer: 1
+    rightAnswer: 2,
+    choices: ["Green", "Purple", "Blue", "Orange"]
   }, {
     question: "What is Donatello's weapon of choice?",
-    answerOptions: ["Gun", "Fists", "The Bo Staff", "Twin Sais"],
-    correctAnswer: 2
+    rightAnswer: 0,
+    choices: ["The Bo Staff", "Gun", "Fists", "Twin Sais"]
   }, {
     question: "What is the one ingredient the turtles (specifically Michelangelo) do NOT like on their pizzas?",
-    answerOptions: ["Anchovies", "Pineapple", "Ham", "Olives"],
-    correctAnswer: 0
+    rightAnswer: 1,
+    choices: ["Pineapple", "Anchovies", "Ham", "Olives"]
   }, {
-    question: "Who does April O'Neil do for work?",
-    answerOptions: ["Admin Assistant", "Boxer", "Chef", "News Reporter"],
-    correctAnswer: 3
+    question: "What is April O'Neil line of work?",
+    rightAnswer: 3,
+    choices: ["Chef", "Boxer", "Admin Assistant", "News Reporter"]
   }, {
     question: "What substance took the turtles from normal reptiles to crime-fighting ninjas?",
-    answerOptions: ["Radiation", "Vaccines", "The Ooze", "Pizza"],
-    correctAnswer: 2
+    rightAnswer: 1,
+    choices: ["Radiation", "The Ooze", "Pizza", "Vaccines"]
   }, {
     question: "What is the name of the group of ninjas led by The Shredder?",
-    answerOptions: ["The Foot Clan", "The Hand Clan", "The Van Buren Clan", "The Shredder Clan"],
-    correctAnswer: 0
+    rightAnswer: 0,
+    choices: ["The Foot Clan", "The Hand Clan", "The Van Buren Clan", "The Shredder Clan "]
   }, {
     question: "What was the name of the fifth Teenage Mutant Ninja Turtle?",
-    answerOptions: ["Becky", "April", "Venus de Milo", "Angela"],
-    correctAnswer: 2
+    rightAnswer: 3,
+    choices: ["Becky", "Angela", "April", "Venus de Milo"]
   }, {
     question: "What is Casey Jones weapon of choice?",
-    answerOptions: ["Hockey Stick", "Fists", "Not a Character", "A Bat Wrapped in Barbed-wire"],
-    correctAnswer: 0
+    rightAnswer: 2,
+    choices: ["A Bat Wrapped in Barbed-wire", "Fists", "Hockey Stick", "Not a Character"]
   }, {
     question: "What is Raphael's bandana color?",
-    answerOptions: ["Green", "Blue", "Purple", "Red"],
-    correctAnswer: 3
+    rightAnswer: 1,
+    choices: ["Green", "Red", "Blue", "Orange,"]
   }, {
     question: "What is Michelangelo's weapon of choice?",
-    answerOptions: ["Swords", "Bat", "Nunchaku", "Grappling Hook"],
-    correctAnswer: 2
+    rightAnswer: 0,
+    choices: ["Nunchaku", "Swords", "Bat", "Grappling Hook"]
   }, {
     question: "The Teenage Mutant Ninja Turtles comic was originally a spoof on what Marvel franchise?",
-    answerOptions: ["Star Wars", "Daredevil", "X-Men", "The Hulk"],
-    correctAnswer: 1
+    rightAnswer: 1,
+    choices: ["Star Wars", "Daredevil", "X-Men", "The Hulk"]
   }, {
     question: "Who are the creators of TMNT?",
-    answerOptions: ["Stan Lee and Steve Ditko", "Peter Laird and Kevin Eastman", "Neil Gaiman and Terry Pratchett", "Holden McNeil and Banky Edwards"],
-    correctAnswer: 1
+    rightAnswer: 3,
+    choices: ["Stan Lee and Steve Ditko", "Neil Gaiman and Terry Pratchett", "Peter Laird and Kevin Eastman", "Holden McNeil and Banky Edwards"]
   }, {
     question: "Who is the Turtle's sensei?",
-    answerOptions: ["Rocksteady", "Shredder", "Splinter", "Monkey D. Luffy"],
-    correctAnswer: 2
+    rightAnswer: 3,
+    choices: ["Rocksteady", "Shredder", "Monkey D. Luffy", "Splinter"]
   }, {
     question: "How long did the '80s cartoon series run?",
-    answerOptions: ["10 years", "5 Years", "20 Years", "3 Years"],
-    correctAnswer: 0
+    rightAnswer: 2,
+    choices: ["5 Years", "20 Years", "10 Years", "3 Years"]
   }, {
     question: "What was the name of the Teenage Mutant Ninja Turtles musical tour that kicked off in 1990?",
-    answerOptions: ["Out of the Shadows Tour", "The Out of the Shell tour", "Tour de Pizza", "4 Brothers Tour"],
-    correctAnswer: 1
+    rightAnswer: 0,
+    choices: ["The Out of the Shell tour", "Out of the Shadows Tour", "Tour de Pizza", "4 Brothers Tour"]
   }];
+
+var currentQuestion;
+var correctAnswer;
+var incorrectAnswer;
+var notAnswered;
+var seconds;
+var time;
+var questionsAnswered;
+var userChoice;
+var choiceText = {
+  correct: "Most excellent, dude, I was thinking the same thing, myself",
+  incorrect: "We turtles don’t know the meaning of the word ‘defeat’.",
+  timeOut: "You don’t flirt with the enemy Leo. You take them down!",
+  done: "Most excellent, dude, I was thinking the same thing, myself."
+}
+
+$("#startBtn").on("click", function () {
+  $(this).hide();
+  newGame();
+});
+$("#restartBtn").on("click", function () {
+  $(this).hide();
+  newGame();
+});
+
+function newGame() {
+  $("#endGameMessage").empty();
+  $("#correctAnswers").empty();
+  $("#incorrectAnswers").empty();
+  $("#incorrectAnswers").empty();
+  $("#notAnswered").empty();
+  currentQuestion = 0;
+  correctAnswer = 0;
+  incorrectAnswer = 0;
+  notAnswered = 0;
+  newQuestion();
+}
+function newQuestion() {
+  $("#message").empty;
+  $("#correctedAnswer").empty;
+  // $("#gif").empty();
+  answered = true;
+  //appends questions and answer to the page
+  $("#currentQuestion").html("Question #: " + (currentQuestion + 1) + "/" + triviaQuestions.length);
+  $(".questionArea").html("<h2>" + triviaQuestions[currentQuestion].question + "</h2>");
+  for (var i = 0; i < 4; i++) {
+    var ansChoices = $("<div>");
+    ansChoices.text(triviaQuestions[currentQuestion].choices[i]);
+    ansChoices.attr({ "data-index": i });
+    ansChoices.addClass("thisChoice");
+    $(".answerChoices").append(ansChoices);
+  }
+
+}
+
+
+
 
 
 
@@ -79,7 +126,7 @@ $(document).ready(function () {
 
 // * You'll create a trivia game that shows only one question until the player answers it or their time runs out.
 
-//   * If the player selects the correct answer, show a screen congratulating them for choosing the correct option.After a few seconds, display the next question-- do this without user input.
+//   * If the player selects the correct answer, show a screen congratulating them for choosing the right option.After a few seconds, display the next question-- do this without user input.
 
 // * The scenario is similar for wrong answers and time - outs.
 
